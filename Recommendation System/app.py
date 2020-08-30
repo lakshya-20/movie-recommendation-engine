4c8279556f29132905a1b933eccf75c1e227234d
Lakshya Bansal <39180629+lakshya-20@users.noreply.github.com>
2020-08-30 17:09:50 +0530

    flask endpoints
    encapsulated code under endpoints, also the previous app.py version is moved to script.py.

--- Recommendation System/app.py
+++ Recommendation System/app.py
@@ -1,35 +1,93 @@
 import pandas as pd
 import numpy as np
-print("Starting...")
-movies_df=pd.read_csv('movies.csv')
-movies_df.drop('title',1,inplace=True)
-movies_df['genres']=movies_df.genres.str.split('|')
-genres_df=movies_df.copy()
-for index,row in movies_df.iterrows():
-    for genre in row['genres']:
-        genres_df.at[index,genre]=1
-genres_df.drop('genres',1,inplace=True)
-genres_df.fillna(0,inplace=True)
-genres_df.to_csv('genres.csv',index=False)
-genres_df=pd.read_csv('genres.csv')
+import pymongo
+import itertools
+from flask import Flask,request, url_for, redirect, render_template
 
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["flick"]
 
-userInput=[
-    {'movieId':34,'rating':5.0},
-    {'movieId':24,'rating':4.0},
-    {'movieId':14,'rating':3.0},
-    {'movieId':4,'rating':2.0},
-    {'movieId':44,'rating':1.0}
-]
-userInput=pd.DataFrame(userInput)
+app=Flask(__name__)
 
-userGenre=genres_df[genres_df['movieId'].isin(userInput['movieId'].tolist())]
-userGenre.drop('movieId',1,inplace=True)
-userGenre.reset_index(drop=True)
-userProfile = userGenre.transpose().dot(userInput.rating.values)
-genreTable=genres_df.copy()
-genreTable.set_index('movieId',inplace=True)
+@app.route('/initialize')
+def initialize():
+    mycol = mydb["genres_data"]
+    mycol.drop()
+    genres_df=pd.read_csv('genres.csv')
+    genre_json=genres_df.to_json(orient='records')
+    genre_json=eval(genre_json)
+    genre_data=genre_json
+    x = mycol.insert_many(genre_data)
+    print("genres data synchronized")
+    mycol = mydb["movies_data"]
+    mycol.drop()
+    movies_df=pd.read_csv('movies.csv')
+    movies_json=movies_df.to_json(orient='records')
+    movies_json=eval(movies_json)
+    movies_data=movies_json
+    movies_data[0]
+    x = mycol.insert_many(movies_data)
+    print("movies data synchronized")
+    return "Initialized"
 
-recommend_df=((genreTable*userProfile).sum(axis=1))/(userProfile.sum())
-recommend_df.sort_values(ascending=False,inplace=True)
-print(recommend_df.head(5))
+@app.route('/newReview')
+def newReview():
+    uid=1
+    movieId=42
+    rating=4
+    mycol = mydb["reviews"]
+    record={ "uid": uid, "movieId": movieId,"rating":rating }
+    mycol.insert_one(record)
+    #generating userInput table
+    userInput=[]
+    for x in mycol.find({"uid":uid},{ "_id": 0}):
+      userInput.append(x)
+    userInput=pd.DataFrame(userInput)
+    userInput=userInput.drop(['uid'], axis = 1) 
+    #fetching genres data from database
+    mycol = mydb["genres_data"]
+    data=[]
+    for x in mycol.find({},{"_id":0}):
+        data.append(x)
+    genres_df=pd.DataFrame(data)
+    #generating userProfile
+    userGenre=genres_df[genres_df['movieId'].isin(userInput['movieId'].tolist())]
+    userGenre.drop('movieId',1,inplace=True)
+    userGenre.reset_index(drop=True)
+    userProfile = userGenre.transpose().dot(userInput.rating.values)
+    #getting new recommendations
+    genreTable=genres_df.copy()
+    genreTable.set_index('movieId',inplace=True)
+    recommend_df=((genreTable*userProfile).sum(axis=1))/(userProfile.sum())
+    recommend_df.sort_values(ascending=False,inplace=True)
+    mycol = mydb["user_recommendation_data"]
+    recommendation_json=eval(recommend_df.to_json())
+    recommendation_data={"uid":1,"recommendation_data":recommendation_json}
+    mycol.delete_one({"uid":uid})
+    x = mycol.insert_one(recommendation_data)
+    return "Review added"
+
+@app.route('/recommendation')
+def recommendation():
+    mycol = mydb["user_recommendation_data"]
+    x=mycol.find_one({"uid":1},{"_id":0})
+    data=x['recommendation_data']
+    # recommend_df=pd.Series(data)
+    # recommend_df.head()
+    data=dict(itertools.islice(data.items(),5))
+    movies_id=list(data.keys())
+
+    mycol=mydb['movies_data']
+
+    movies_data=[]
+
+    for i in mycol.find({},{"_id":0}):
+        if(str(i["movieId"]) in movies_id):
+            movies_data.append(i)
+        #print(i["movieId"])
+    movies_data=tuple(movies_data)
+    re=(1,2,3)
+    return "done"
+
+if __name__=='__main__':
+    app.run(debug=True)
